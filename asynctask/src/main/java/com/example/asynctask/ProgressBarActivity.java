43f7d9c8382d5b0a6a98f58fa3d8d69329cad011
JavaCoffee <1095908464@qq.com>
2016-08-04 21:26:18 +0800

    异步任务与activity同步

--- asynctask/src/main/java/com/example/asynctask/ProgressBarActivity.java
+++ asynctask/src/main/java/com/example/asynctask/ProgressBarActivity.java
@@ -9,6 +9,7 @@ import android.widget.ProgressBar;
 /**
  * Created by DHW on 2016/8/4.
  */
+
 public class ProgressBarActivity extends AppCompatActivity{
     private ProgressBar progressBar;
     private MyAsyncTask myAsyncTask;
@@ -20,11 +21,24 @@ public class ProgressBarActivity extends AppCompatActivity{
         myAsyncTask = new MyAsyncTask();
         myAsyncTask.execute();
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(myAsyncTask != null && myAsyncTask.getStatus() == AsyncTask.Status.RUNNING){
+            //只是将异步任务标记为取消状态
+            myAsyncTask.cancel(true);
+        }
+    }
+
     class MyAsyncTask extends AsyncTask<Void,Integer,Void>{
 
         @Override
         protected Void doInBackground(Void... voids) {
             for(int i = 0;i < 100;i++){
+                if(isCancelled()){
+                    break;
+                }
                 publishProgress(i);
                 try {
                     Thread.sleep(300);
@@ -38,6 +52,9 @@ public class ProgressBarActivity extends AppCompatActivity{
         @Override
         protected void onProgressUpdate(Integer... values) {
             super.onProgressUpdate(values);
+            if(isCancelled()){
+                return;
+            }
             progressBar.setProgress(values[0]);
         }
     }
