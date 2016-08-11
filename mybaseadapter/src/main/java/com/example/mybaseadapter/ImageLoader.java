2eb03da5d5849bae541a34bb0255c08802e45780
JavaCoffee <1095908464@qq.com>
2016-08-11 23:47:54 +0800

    Thread和AsyncTask加载网络图片LruCache缓存

--- mybaseadapter/src/main/java/com/example/mybaseadapter/ImageLoader.java
+++ mybaseadapter/src/main/java/com/example/mybaseadapter/ImageLoader.java
@@ -5,6 +5,7 @@ import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Message;
+import android.util.LruCache;
 import android.widget.ImageView;
 
 import java.io.BufferedInputStream;
@@ -20,6 +21,31 @@ public class ImageLoader {
 
     private ImageView mimageView;
     private String mUrl;
+    private LruCache<String, Bitmap> mCaches;
+
+    public ImageLoader() {
+        int maxMemory = (int) Runtime.getRuntime().maxMemory();
+        int cacheSize = maxMemory / 4;
+        mCaches = new LruCache<String, Bitmap>(cacheSize) {
+            @Override
+            //在每次存入缓存中调用
+            protected int sizeOf(String key, Bitmap value) {
+                return value.getByteCount();
+            }
+        };
+    }
+
+    public void addBitmapToCache(String url, Bitmap bitmap) {
+        if (getBitmapFromCache(url) == null) {
+            mCaches.put(url, bitmap);
+        }
+    }
+
+    public Bitmap getBitmapFromCache(String url) {
+        return mCaches.get(url);
+    }
+
+
     private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
@@ -68,20 +94,35 @@ public class ImageLoader {
         }
         return null;
     }
-
-    public void showImageByAsyncTask(ImageView imageView,final String url){
-       new NewAsyncTask(imageView).execute(url);
+//异步方式加载图片
+    public void showImageByAsyncTask(ImageView imageView, final String url) {
+        //从缓存中取出图片
+        Bitmap bitmap = getBitmapFromCache(url);
+        //如果缓存中没有该url对应的图片，则从网络下载
+        if (bitmap == null) {
+            new NewAsyncTask(imageView).execute(url);
+        }else {
+            imageView.setImageBitmap(bitmap);
+        }
     }
-    private class NewAsyncTask extends AsyncTask<String,Void,Bitmap>{
+
+    private class NewAsyncTask extends AsyncTask<String, Void, Bitmap> {
 
         private ImageView mImageView;
+
         public NewAsyncTask(ImageView imageView) {
             mImageView = imageView;
         }
 
         @Override
         protected Bitmap doInBackground(String... strings) {
-            return getBitmapFromURL(strings[0]);
+            Bitmap bitmap = getBitmapFromURL(strings[0]);
+            String url = strings[0];
+            if (bitmap != null){
+                //将下载的图片存入缓存
+                addBitmapToCache(url,bitmap);
+            }
+            return bitmap;
         }
 
         @Override
--- mybaseadapter/src/main/java/com/example/mybaseadapter/MyAdapter.java
+++ mybaseadapter/src/main/java/com/example/mybaseadapter/MyAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -13,6 +14,7 @@ import java.util.List;
 /**
  * Created by DHW on 2016/8/3.
  */
+
 /**
  * ViewHolder优化BaseAdater思路
  * 创建Bean对象，用于封装数据
@@ -21,15 +23,18 @@ import java.util.List;
  * 判断convertView,为空则创建，并设置tag,否则通过tag来取出来给ViewHolder
  * 给ViewHolder中的控件设置数据
  */
-public class MyAdapter extends BaseAdapter {
+public class MyAdapter extends BaseAdapter implements AbsListView.OnScrollListener {
 
     private List<ItemBean> mList;
     private LayoutInflater mInflater;
     private Context context;
+    private ImageLoader mImageLoader;
+    private int mStart, mEnd;
 
     public MyAdapter(Context context, List<ItemBean> List) {
         mList = List;
         mInflater = LayoutInflater.from(context);
+        mImageLoader = new ImageLoader();//此方法避免每次创建imageloader时重新创建LruCache
     }
 
     @Override
@@ -74,14 +79,14 @@ public class MyAdapter extends BaseAdapter {
 
         //文艺式*************************************************ViewHolder保存控件对象//
         ViewHolder viewHolder;
-        if(view == null){
+        if (view == null) {
             viewHolder = new ViewHolder();
             view = mInflater.inflate(R.layout.item, null);
             viewHolder.imageView = (ImageView) view.findViewById(R.id.image);
             viewHolder.title = (TextView) view.findViewById(R.id.tv_title);
             viewHolder.content = (TextView) view.findViewById(R.id.tv_content);
             view.setTag(viewHolder);   //将查找到的控件id保存在viewHolder中，避免多次findViewById寻找控件
-        }else {
+        } else {
             viewHolder = (ViewHolder) view.getTag();
         }
         viewHolder.imageView.setImageResource(R.mipmap.ic_launcher);
@@ -92,12 +97,30 @@ public class MyAdapter extends BaseAdapter {
         //使用Thread加载网络图片
         //new ImageLoader().showImageByThread(viewHolder.imageView,url);
         //使用异步加载网络图片
-        new ImageLoader().showImageByAsyncTask(viewHolder.imageView,url);
+        mImageLoader.showImageByAsyncTask(viewHolder.imageView, url);
         viewHolder.title.setText(mList.get(i).ItemTitle);
         viewHolder.content.setText(mList.get(i).ItemContent);
         return view;
     }
-    class ViewHolder{
+
+    @Override
+    public void onScrollStateChanged(AbsListView absListView, int scrollState) {
+        if (scrollState == SCROLL_STATE_IDLE) {
+            //加载可见项
+
+        } else {
+            //停止任务
+        }
+    }
+
+    @Override
+    public void onScroll(AbsListView absListView, int i, int i1, int i2) {
+        //i代表第一个可见元素，i1代表当前可见元素长度
+        mStart = i;
+        mEnd = i + i1;
+    }
+
+    class ViewHolder {
         private ImageView imageView;
         private TextView title;
         private TextView content;
